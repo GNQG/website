9df2977085384df57fdd2d62bacddc568d63aa62
GNQG <my.t.gnq+dev@gmail.com>
2019-03-25 14:30:18 +0900

    add about/ page

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,10 +2,8 @@ module.exports = {
     title: "website",
     description: "by ginkgo",
     themeConfig: {
-        nav: [
-            { text: "home", link: "/" },
-        ],
-        repo: 'https://github.com/GNQG/website',
-        repoLabel: 'Repository',
-    },
+        nav: [{ text: "home", link: "/" }, { text: "about", link: "/about/" }],
+        repo: "https://github.com/GNQG/website",
+        repoLabel: "Repository"
+    }
 };
