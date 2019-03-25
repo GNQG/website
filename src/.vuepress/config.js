4cfee4ca04782659161fc4f282dafa8b0a737869
GNQG <my.t.gnq+dev@gmail.com>
2019-03-25 14:41:00 +0900

    add (local) blog plugin

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,8 +1,23 @@
 module.exports = {
     title: "website",
     description: "by ginkgo",
+    plugins: [
+        [
+            require("./plugins/my-blog"),
+            {
+                postsDir: "blog/_posts",
+                categoryIndexPageUrl: "/blog/category/",
+                tagIndexPageUrl: "/blog/tag/",
+                permalink: "/blog/entry/:slug"
+            }
+        ]
+    ],
     themeConfig: {
-        nav: [{ text: "home", link: "/" }, { text: "about", link: "/about/" }],
+        nav: [
+            { text: "home", link: "/" },
+            { text: "about", link: "/about/" },
+            { text: "blog", link: "/blog/" }
+        ],
         repo: "https://github.com/GNQG/website",
         repoLabel: "Repository"
     }
