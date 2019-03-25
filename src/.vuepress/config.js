4851c0f3864e2c95edace9d27e62a64b745c5312
GNQG <my.t.gnq+dev@gmail.com>
2019-03-25 14:15:51 +0900

    create VuePress project with default theme

---
+++ src/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: "website",
+    description: "by ginkgo",
+    themeConfig: {
+        nav: [
+            { text: "home", link: "/" },
+        ],
+        repo: 'https://github.com/GNQG/website',
+        repoLabel: 'Repository',
+    },
+};
---
+++ src/index.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroText: Hello world!
+tagline: by ginkgo
+---
+
+<!--
+<pre v-highlightjs><code class="json">
+{{ JSON.stringify($site, null, 2) | pretty }}
+</code></pre>
+-->
