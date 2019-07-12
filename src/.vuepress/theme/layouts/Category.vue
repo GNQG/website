c7b5c54d8c497cfa58e3700c8536dccb258c6136
GNQG <my.t.gnq+dev@gmail.com>
2019-07-13 07:37:21 +0900

    fix bugs on Tag and Category

--- src/.vuepress/theme/layouts/Category.vue
+++ src/.vuepress/theme/layouts/Category.vue
@@ -1,8 +1,8 @@
 <template>
     <BlogLayoutBase>
         <h1>{{$page.frontmatter.title}}</h1>
-        <ul id="default-layout">
-            <li v-if="$category.list" v-for="category in $category.list">
+        <ul v-if="$category.list" id="default-layout">
+            <li v-for="category in $category.list">
                 <router-link class="page-link" :to="category.path">{{ category.name }}</router-link>
                 ({{category.pages.length}})
             </li>
