2620cd6365652df62bb6e6c4b4b30909133a460f
GNQG <my.t.gnq+dev@gmail.com>
2019-08-01 00:15:54 +0900

    add setting for icon font(Fork Awesome)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -114,6 +114,17 @@ module.exports = {
                 rel: "stylesheet",
                 href: `https://cdnjs.cloudflare.com/ajax/libs/KaTeX/${katex.version}/katex.min.css`
             }
+        ],
+        [
+            "link",
+            {
+                rel: "stylesheet",
+                href:
+                    "https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css",
+                integrity:
+                    "sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=",
+                crossorigin: "anonymous"
+            }
         ]
     ]
 };
