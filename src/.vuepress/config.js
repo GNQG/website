63a33957cd07c88ba49d2012e59b4a52ddfa1a8a
GNQG <my.t.gnq+dev@gmail.com>
2019-08-02 18:16:19 +0900

    change blog post directory

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
                     {
                         id: "posts",
                         name: "blog",
-                        dirname: "blog/_posts",
+                        dirname: "_blogposts",
                         path: "/blog/",
                         itemPermalink: "/blog/:slug/",
                         layout: "BlogPager",
