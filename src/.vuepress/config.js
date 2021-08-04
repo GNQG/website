fafde4580e08b7fe37d42eea00fa7dabfa596d33
GNQG <my.t.gnq+dev@gmail.com>
2021-08-04 14:07:32 +0900

    update dependencies

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -130,8 +130,7 @@ module.exports = {
             "link",
             {
                 rel: "stylesheet",
-                href:
-                    "https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css",
+                href: "https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css",
                 integrity:
                     "sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=",
                 crossorigin: "anonymous",
